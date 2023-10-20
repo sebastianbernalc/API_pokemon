0900a6399c2ba5f5596f9314fea7cc1c16b06311
Sebastian Bernal <sebastian.bernalc@udea.edu.co>
2023-10-20 00:13:45 -0500

    Se agrego primera API de pokemon sin estilo aun

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const url = "https://reqres.in/api/users?page=2"
+
+
+
 const boton = document.querySelector("button");
 boton.addEventListener(
   "click",
   () => {
-    getDataFromApiResponse(consumeApiWithAxios(url));
+
+    const searchInput = document.getElementById("search-input"); // Get the input field
+
+
+    getDataFromApiResponse(consumeApiWithAxios(searchInput.value));
+    change_style();
+
   }
 );
 
 
 
-async function consumeApiWithAxios(url) {
+async function consumeApiWithAxios(page) {
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+  const url = `${baseUrl}/${page}`;
+  console.log(`la url de la api es: ${url}`);
   try {
     const response = await axios.get(url);
     console.log(`la petición a la api se completo correctamente con status: ${response.status}`);
@@ -20,38 +31,60 @@ async function consumeApiWithAxios(url) {
 }
 
 
-/* Acceso a los datos de la respuesta */
+/* Acceso a los datoss de la respuesta */
 
 
 async function getDataFromApiResponse(response) {
   const apiResponse = await response;
-  const datos = apiResponse.data;
-  const body = document.querySelector("body");
+  const imagen = apiResponse.sprites;
+  const nombre = apiResponse.name;
+  const edad = apiResponse.order;
+  const body = document.querySelector("header");
   const contenedor = document.createElement("div");
   contenedor.setAttribute("class", "container");
-  for (dato of datos) {
-    console.log(dato);
-    const user = document.createElement("div");
-    user.setAttribute("class", "user");
-    const userImg = document.createElement("div");
-    userImg.setAttribute("class", "user-pic");
-    const img = document.createElement("img");
-    img.setAttribute("src", dato.avatar);
-    const userName = document.createElement("div");
-    userName.setAttribute("class", "user-name");
-    const userEmail = document.createElement("div");
-    userEmail.setAttribute("class", "user-mail");
-    const fullName = document.createElement("p");
-    fullName.innerHTML = `<strong>Name: </strong> ${dato.first_name} ${dato.last_name}`;
-    const email = document.createElement("p");
-    email.innerHTML = `<strong>Email: </strong>${dato.email}`;
-    userName.appendChild(fullName);
-    userEmail.appendChild(email);
-    userImg.appendChild(img);
-    user.appendChild(userImg);
-    user.appendChild(userName);
-    user.appendChild(userEmail);
-    contenedor.appendChild(user);
-  }
+
+
+  const user = document.createElement("div");
+  user.setAttribute("class", "user");
+
+
+  const userImg = document.createElement("div");
+  userImg.setAttribute("class", "user-pic");
+  const img = document.createElement("img");
+  img.style.maxWidth = "200px";
+  img.setAttribute("src", imagen.other.home.front_default);
+  const userName = document.createElement("div");
+  userName.setAttribute("class", "user-name");
+  const userEmail = document.createElement("div");
+  userEmail.setAttribute("class", "user-mail");
+  const fullName = document.createElement("p");
+  fullName.innerHTML = `<strong>Name: </strong> ${nombre}`;
+  const email = document.createElement("p");
+  email.innerHTML = `<strong>Email: </strong>${edad}`;
+  userName.appendChild(fullName);
+  userEmail.appendChild(email);
+  userImg.appendChild(img);
+  user.appendChild(userImg);
+  user.appendChild(userName);
+  user.appendChild(userEmail);
+  contenedor.appendChild(user);
+
   body.appendChild(contenedor);
+}
+
+async function change_style() {
+  const logo = document.querySelector(".logo");
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const searchPadding = document.querySelector(".search");
+  searchPadding.style.padding = "10px";
+  searchButton.style.width = "80px";
+  searchButton.style.height = "30px";
+  searchButton.style.fontSize = "1px";
+  searchInput.style.width = "180px"; /* Aumenta el ancho del cuadro de búsqueda según tus preferencias */
+  searchInput.style.height = "30px";
+  searchInput.style.fontSize = "14px";
+
+  logo.style.width = "35%";
+
 }
