59724bb9dd8c9687a60b26363fe91b0ee7f1573c
Sebastian Bernal <sebastian.bernalc@udea.edu.co>
2023-10-20 23:36:21 -0500

    Se agrego condiciones de botones y api consumiendo

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
-
-
 const boton = document.querySelector("button");
+const text_input = document.querySelector("input");
+
+text_input.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+      boton.click(); 
+  }
+});
+
 boton.addEventListener(
   "click",
   () => {
-
-    const searchInput = document.getElementById("search-input"); // Get the input field
-
-
-    getDataFromApiResponse(consumeApiWithAxios(searchInput.value));
-    change_style();
+    remove_card()
+    const searchInput = document.getElementById("search-input");  
+    consumeApiWithAxios(searchInput.value);
+    
 
   }
 );
@@ -19,14 +22,21 @@ boton.addEventListener(
 
 async function consumeApiWithAxios(page) {
   const baseUrl = "https://pokeapi.co/api/v2/pokemon";
-  const url = `${baseUrl}/${page}`;
+  const url = `${baseUrl}/${page.toLowerCase()}`;
+  flag = true;
   console.log(`la url de la api es: ${url}`);
   try {
     const response = await axios.get(url);
     console.log(`la petición a la api se completo correctamente con status: ${response.status}`);
+    getDataFromApiResponse(response.data)
+    change_style();
     return await response.data;
   } catch (error) {
     console.error(`fallo la petición a la api con error: ${error.message}`);
+    return_style();
+    remove_card()
+    alert("Pokemon no valido, intenta otra vez!");
+    return flag;
   }
 }
 
@@ -88,3 +98,25 @@ async function change_style() {
   logo.style.width = "35%";
 
 }
+
+async function return_style() {
+  const logo = document.querySelector(".logo");
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const searchPadding = document.querySelector(".search");
+  searchPadding.style.padding = "50px";
+  searchButton.style.width = "100px";
+  searchButton.style.height = "60px";
+  searchButton.style.fontSize = "16px";
+  searchInput.style.width = "400px"; /* Aumenta el ancho del cuadro de búsqueda según tus preferencias */
+  searchInput.style.height = "60px";
+  searchInput.style.fontSize = "16px";
+  logo.style.width = "50%";
+}
+
+async function remove_card() {
+  const card = document.querySelector(".container");
+  card.remove();
+}
+
+
